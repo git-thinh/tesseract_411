fa8c20d7d4a137812fb5a3d04b32a1db431789cc
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-05-04 09:56:59 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tesseract_411
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
